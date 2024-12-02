ab4bcff4c1aa5a4469df919e094292545f2337bf
Aldo Navarrete <aldo.navarrete@fi.unam.edu>
2024-12-02 12:01:06 -0600

    add script to make folders (disks)

---
+++ db-create-scripts/e-01a-crea-loop-devices-host-root.sh
@@ -0,0 +1,23 @@
+#@Autor: <Nombre del autor o autores> 
+#@Fecha creación: <Fecha de creación> 
+#@Descripción: Crear las 10 carpetas que se van a utilizar para la simulación de los 10 discos duros
+
+# Whenever this scripts fail it will stop
+set -e
+
+#!/bin/bash
+dba_proyecto_final_dir="/unam/bda/proyecto-final"
+
+echo "1. Creando los directorios de los discos duros..."  
+
+for i in {1..9}
+do
+    mkdir -p "${dba_proyecto_final_dir}/d1$i"
+done
+
+mkdir -p "${dba_proyecto_final_dir}/d20"
+
+echo "2. Mostrando la creación de los directorios"
+
+ls -l "${dba_proyecto_final_dir}"
+
