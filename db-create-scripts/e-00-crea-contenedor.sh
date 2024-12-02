6d1ce9735a415c60e3cc66fadc4ffb05333169c1
Aldo Navarrete <aldo.navarrete@fi.unam.edu>
2024-12-02 11:01:45 -0600

    fix container hostname

--- db-create-scripts/e-00-crea-contenedor.sh
+++ db-create-scripts/e-00-crea-contenedor.sh
@@ -6,7 +6,7 @@ docker run -i -t \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
 -v /unam:/unam \
 --name c3-bda-anz-dnh \
---hostname c3-bda-anz-dnh.fi.unam \
+--hostname d3-bda-anz-dnh.fi.unam \
 --expose 1521 \
 --expose 5500 \
 --shm-size=2g \
