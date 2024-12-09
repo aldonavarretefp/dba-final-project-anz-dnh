00f60f5e830a75c376595fe068f85c0c643cd752
Aldo Navarrete <aldo.navarrete@fi.unam.edu>
2024-12-08 18:13:29 -0600

    add s-08

--- 1.3-db-create-scripts/s-08-crea-pdb-ordinario.sql
+++ 1.3-db-create-scripts/s-08-crea-pdb-ordinario.sql
@@ -1,24 +1,38 @@
---@Autor:          Jorge A. Rodriguez C
---@Fecha creación:  dd/mm/yyyy
---@Descripción: Creación de una PDB
+--@Autor:          Jorge A. Rodriguez C TODO
+--@Fecha creación:  dd/mm/yyyy TODO
+--@Descripción: Creación de una PDB TODO
+
+whenever sqlerror exit rollback;
 
 Prompt Conectando como sys
 connect sys/system2 as sysdba
 
-Prompt creando PDB
---#TODO
-create pluggable database naproynu
+Prompt creando PDB 1...
+
+create pluggable database naproynu_modulo_1
   admin user anz_admin_dnh identified by anz_admin_dnh
   path_prefix = '/opt/oracle/oradata/FREE'
-  file_name_convert = ('/pdbseed/', '/naproynu/');
---TODO#
+  file_name_convert = ('/pdbseed/', '/naproynu_modulo_1/');
 
-Prompt abrir la PDB
---#TODO
-alter pluggable database naproynu open;
---TODO#
+Prompt abrir la PDB 1...
+alter pluggable database naproynu_modulo_1 open;
 
-Prompt guardar el estado de la PDB
---#TODO
-alter pluggable database naproynu save state;
---TODO#
+Prompt guardar el estado de la PDB 1...
+
+alter pluggable database naproynu_modulo_1 save state;
+
+Prompt Creando PDB 2...
+
+create pluggable database naproynu_modulo_2
+  admin user anz_admin_dnh identified by anz_admin_dnh
+  path_prefix = '/opt/oracle/oradata/FREE'
+  file_name_convert = ('/pdbseed/', '/naproynu_modulo_2/');
+
+Prompt abrir la PDB 2...
+alter pluggable database naproynu_modulo_2 open;
+
+Prompt guardar el estado de la PDB 2...
+alter pluggable database naproynu_modulo_2 save state;
+
+exit;
+
