e3474f243fbbe52e5558324f66ea480952c610af
Aldo Navarrete <aldo.navarrete@fi.unam.edu>
2024-12-08 18:26:11 -0600

    update e-09

--- 1.3.10-db-fra-enable-scripts/e-09-creando-dir-fra-oracle.sh
+++ 1.3.10-db-fra-enable-scripts/e-09-creando-dir-fra-oracle.sh
@@ -6,14 +6,15 @@
 # ^^ Permite transformar la variable ORACLE_SID a mayúsculas
 #cat permite mostrar el contenido de un archivo
 
-echo "1. Creando un archivo de parámetros básico"
-export ORACLE_SID=naproynu
+set -e
+export ORACLE_SID=free
 
+echo "1. Creando un archivo de parámetros básico"
 pfile=$ORACLE_HOME/dbs/init${ORACLE_SID}.ora
 dba_proyecto_final_dir="/unam/bda/proyecto-final"
 
 echo "2. Verificando la existencia de la carpeta del proyecto final..."
-#Verifica si la carpeta fast-recovery-area ya existe
+echo "Verifica si la carpeta fast-recovery-area ya existe..."
 if [ ! -d "${dba_proyecto_final_dir}/fast-recovery-area" ]; then
   #Si no existe, crea la carpeta
   echo "Creando directorio para la FRA..."
@@ -23,26 +24,25 @@ else
   echo "La carpeta 'fast-recovery-area' ya existe en ${dba_proyecto_final_dir}/"
 fi 
 
-if [ -f "${pfile}" ]; then
-  read -p "El archivo ${pfile} ya existe, [enter] para sobrescribir"
-fi;
+# if [ -f "${pfile}" ]; then
+#   read -p "El archivo ${pfile} ya existe, [enter] para sobrescribir"
+# fi;
 
-echo \
-"db_name='${ORACLE_SID}'
-memory_target=1G
-db_recovery_file_dest_size=1G
-db_recovery_file_dest='${dba_proyecto_final_dir}/fast-recovery-area'
-db_flashback_retention_target=10080  
-" >$pfile
+# echo \
+# "
+# db_recovery_file_dest_size=1G
+# db_recovery_file_dest='${dba_proyecto_final_dir}/fast-recovery-area'
+# db_flashback_retention_target=10080
+# " >> $pfile
 
-echo "Listo!"
-echo "Comprobando la existencia del archivo PFILE..."
-echo ""
+# echo "Listo!"
+# echo "Comprobando la existencia del archivo PFILE..."
+# echo ""
 
-ls -l $ORACLE_HOME/dbs/init${ORACLE_SID}.ora
+# ls -l $ORACLE_HOME/dbs/init${ORACLE_SID}.ora
 
-echo ""
-echo "Mostrando el contenido del archivo PFILE..."
-echo ""
+# echo ""
+# echo "Mostrando el contenido del archivo PFILE..."
+# echo ""
 
-cat ${pfile}
+# cat ${pfile}
