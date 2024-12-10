f5e3d60c75a4384cff983ab11041873efecb369a
Diego Ignacio Núñez Hernández <diego.nunezh@ingenieria.unam.edu>
2024-12-09 23:22:24 -0600

    Update DISH_TYPE.sql

--- 1.3.13-carga-datos/DISH_TYPE.sql
+++ 1.3.13-carga-datos/DISH_TYPE.sql
@@ -7,8 +7,8 @@ set serveroutput on
 alter session set container = naproynu_modulo_2;
 connect usermod2/usermod2@naproynu_modulo_2 
 
-insert into DISH_TYPE (DISH_TYPE_ID, DISH_TYPE_TYPE) values (1, 'entradas');
-insert into DISH_TYPE (DISH_TYPE_ID, DISH_TYPE_TYPE) values (2, 'sopas');
-insert into DISH_TYPE (DISH_TYPE_ID, DISH_TYPE_TYPE) values (3, 'platos fuertes');
-insert into DISH_TYPE (DISH_TYPE_ID, DISH_TYPE_TYPE) values (4, 'postres');
-insert into DISH_TYPE (DISH_TYPE_ID, DISH_TYPE_TYPE) values (5, 'bebidas');
+insert into DISH_TYPE (DISH_TYPE_ID, DISH_TYPE_TYPE) values (1, 'ENTRY');
+insert into DISH_TYPE (DISH_TYPE_ID, DISH_TYPE_TYPE) values (2, 'SOUP');
+insert into DISH_TYPE (DISH_TYPE_ID, DISH_TYPE_TYPE) values (3, 'STRONG_MEAL');
+insert into DISH_TYPE (DISH_TYPE_ID, DISH_TYPE_TYPE) values (4, 'DESSERT');
+insert into DISH_TYPE (DISH_TYPE_ID, DISH_TYPE_TYPE) values (5, 'DRINK');
