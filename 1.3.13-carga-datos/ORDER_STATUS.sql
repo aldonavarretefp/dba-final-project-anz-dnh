819aee42b7bc0976e9b316000b123c614ff8c45e
Diego Ignacio Núñez Hernández <diego.nunezh@ingenieria.unam.edu>
2024-12-09 23:29:06 -0600

    Update ORDER_STATUS.sql

--- 1.3.13-carga-datos/ORDER_STATUS.sql
+++ 1.3.13-carga-datos/ORDER_STATUS.sql
@@ -7,10 +7,10 @@ set serveroutput on
 alter session set container = naproynu_modulo_2;
 connect usermod2/usermod2@naproynu_modulo_2 
 
-insert into ORDER_STATUS (ORDER_REVIEW_ID, ORDER_STATUS) values (1, 'REGISTERED');
-insert into ORDER_STATUS (ORDER_REVIEW_ID, ORDER_STATUS) values (2, 'PAID');
-insert into ORDER_STATUS (ORDER_REVIEW_ID, ORDER_STATUS) values (3, 'IN_PROCESS');
-insert into ORDER_STATUS (ORDER_REVIEW_ID, ORDER_STATUS) values (4, 'SENT_TO_DEALER');
-insert into ORDER_STATUS (ORDER_REVIEW_ID, ORDER_STATUS) values (5, 'DELIVERED');
-insert into ORDER_STATUS (ORDER_REVIEW_ID, ORDER_STATUS) values (6, 'CANCELED');
-insert into ORDER_STATUS (ORDER_REVIEW_ID, ORDER_STATUS) values (7, 'REJECTED');
+insert into ORDER_STATUS (ORDER_STATUS_ID, ORDER_STATUS) values (1, 'REGISTERED');
+insert into ORDER_STATUS (ORDER_STATUS_ID, ORDER_STATUS) values (2, 'PAID');
+insert into ORDER_STATUS (ORDER_STATUS_ID, ORDER_STATUS) values (3, 'IN_PROCESS');
+insert into ORDER_STATUS (ORDER_STATUS_ID, ORDER_STATUS) values (4, 'SENT_TO_DEALER');
+insert into ORDER_STATUS (ORDER_STATUS_ID, ORDER_STATUS) values (5, 'DELIVERED');
+insert into ORDER_STATUS (ORDER_STATUS_ID, ORDER_STATUS) values (6, 'CANCELED');
+insert into ORDER_STATUS (ORDER_STATUS_ID, ORDER_STATUS) values (7, 'REJECTED');
