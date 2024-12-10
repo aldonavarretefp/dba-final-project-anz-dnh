5ab03844fb60f137269da3e71e3e447d93ee152d
Diego Ignacio Núñez Hernández <diego.nunezh@ingenieria.unam.edu>
2024-12-09 21:14:53 -0600

    Update BANK.sql

--- 1.3.13-carga-datos/BANK.sql
+++ 1.3.13-carga-datos/BANK.sql
@@ -7,9 +7,9 @@ set serveroutput on
 alter session set container = naproynu_modulo_1;
 connect usermod1/usermod1@naproynu_modulo_1 
 
-insert into BANK (BANK_ID, BANK_NAME) values (1, 'Community Credit Union');
+insert into BANK (BANK_ID, BANK_NAME) values (1, 'Community Credit');
 insert into BANK (BANK_ID, BANK_NAME) values (2, 'First National Bank');
 insert into BANK (BANK_ID, BANK_NAME) values (3, 'City Bank,');
 insert into BANK (BANK_ID, BANK_NAME) values (4, 'Regional Trust Bank');
-insert into BANK (BANK_ID, BANK_NAME) values (5, 'Capital Savings and Loan');
+insert into BANK (BANK_ID, BANK_NAME) values (5, 'Capital Savings');
 insert into BANK (BANK_ID, BANK_NAME) values (6, 'Capital One');
