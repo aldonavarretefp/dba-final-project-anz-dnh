dd03a73d20e03a02061065794a9a339501a897c2
Diego Ignacio Núñez Hernández <diego.nunezh@ingenieria.unam.edu>
2024-12-10 17:32:25 -0600

    nuevo script: genera 30 MB en redo logs

--- 1.3-db-create-scripts/e-01-crea-loop-devices-host-root.sh
+++ 1.3-db-create-scripts/e-01-crea-loop-devices-host-root.sh
@@ -10,14 +10,11 @@ dba_proyecto_final_dir="/unam/bda/proyecto-final"
 
 echo "1. Creando directorio disk-images"
 
-#--TODO
 mkdir -p "${dba_proyecto_final_dir}/disk-images"
-#TODO--
 
 echo "2,3. Crear archivos img"
 cd "${dba_proyecto_final_dir}/disk-images"
 
-#--TODO
 if [ -f disk1.img ]; then
     echo "El archivo disk1.img ya existe"
 else
@@ -35,43 +32,35 @@ if [ -f disk3.img ]; then
 else
     dd if=/dev/zero of=disk3.img bs=50M count=10
 fi
-#TODO---
 
 echo "4. Mostrando la creación de los archivos"
 du -sh disk*.img 
 
 echo "5. Creando Loop devices "
-#--TODO
+
 losetup -fP disk1.img
 losetup -fP disk2.img
 losetup -fP disk3.img
-#TODO--
 
 echo "Mostrando la creación de loop devices"
 losetup -a
 
 echo "6. Dando formato ext4 a los arhivos img"
 
-#--TODO
 mkfs.ext4 disk1.img 
 mkfs.ext4 disk2.img
 mkfs.ext4 disk3.img
-#TODO--
 
 echo "7. Creando directorios para usarse como puntos de montaje"
-#TODO--
+
 mkdir -p "${dba_proyecto_final_dir}/disk/d01"
 mkdir -p "${dba_proyecto_final_dir}/disk/d02"
 mkdir -p "${dba_proyecto_final_dir}/disk/d03"
-#TODO--
 
 echo "Editando archivo /etc/fstab"
 
-#--TODO
 echo "# loop devices para el el proyecto" >> /etc/fstab
 echo "${dba_proyecto_final_dir}/disk-images/disk1.img ${dba_proyecto_final_dir}/disk/d01 auto loop 0 0" >> /etc/fstab
 echo "${dba_proyecto_final_dir}/disk-images/disk2.img ${dba_proyecto_final_dir}/disk/d02 auto loop 0 0" >> /etc/fstab
 echo "${dba_proyecto_final_dir}/disk-images/disk3.img ${dba_proyecto_final_dir}/disk/d03 auto loop 0 0" >> /etc/fstab
 echo "Listo!"
-
-
---
+++ 1.3.12-planeacion-esquema-respaldos/genera-redo.sql
@@ -0,0 +1,57 @@
+--@Autor: Aldo Yael Navarrete Zamora, Diego Ignacio Núñez Hernández
+--@Fecha creación:  07/12/2024
+--@Descripción:  Genera operaciones update, para generar datos REDO
+
+SET SERVEROUTPUT ON
+
+DECLARE
+  v_count NUMBER;
+BEGIN
+  FOR i IN 1..300 LOOP
+    -- Actualizar CLIENT
+    v_count := 0;
+    FOR r IN (SELECT CLIENT_USER_ID, CLIENT_NAME, CLIENT_LASTNAME, CLIENT_BIRTHDATE, 
+                     CLIENT_ADDRESS, CLIENT_LATITUDE, CLIENT_LONGITUDE 
+              FROM CLIENT) LOOP
+      UPDATE CLIENT
+      SET CLIENT_NAME = r.CLIENT_NAME,
+          CLIENT_LASTNAME = r.CLIENT_LASTNAME,
+          CLIENT_ADDRESS = r.CLIENT_ADDRESS,
+          CLIENT_LATITUDE = r.CLIENT_LATITUDE,
+          CLIENT_LONGITUDE = r.CLIENT_LONGITUDE,
+      WHERE CLIENT_USER_ID = r.CLIENT_USER_ID;
+      v_count := v_count + SQL%ROWCOUNT;
+    END LOOP;
+    DBMS_OUTPUT.PUT_LINE('Registros actualizados en CLIENT: ' || v_count);
+
+    -- Actualizar DEALER
+    v_count := 0;
+    FOR r IN (SELECT DEALER_USER_ID, DEALER_NAME, DEALER_LASTNAME, DEALER_LICENSE_NUMBER, 
+                     DEALER_LICENSE_STARTDATE, DEALER_LICENSE_END_DATE, DEALER_MOTORCYCLE_PLATE
+              FROM DEALER) LOOP
+      UPDATE DEALER
+      SET DEALER_NAME = r.DEALER_NAME,
+          DEALER_LASTNAME = r.DEALER_LASTNAME,
+          DEALER_LICENSE_NUMBER = r.DEALER_LICENSE_NUMBER,
+          DEALER_LICENSE_STARTDATE = r.DEALER_LICENSE_STARTDATE,
+          DEALER_LICENSE_END_DATE = r.DEALER_LICENSE_END_DATE,
+          DEALER_MOTORCYCLE_PLATE = r.DEALER_MOTORCYCLE_PLATE,
+      WHERE DEALER_USER_ID = r.DEALER_USER_ID;
+      v_count := v_count + SQL%ROWCOUNT;
+    END LOOP;
+    DBMS_OUTPUT.PUT_LINE('Registros actualizados en DEALER: ' || v_count);
+
+    -- Actualizar PROVIDER
+    v_count := 0;
+    FOR r IN (SELECT PROVIDER_USER_ID, PROVIDER_NAME, PROVIDER_DESCRIPTION 
+              FROM PROVIDER) LOOP
+      UPDATE PROVIDER
+      SET PROVIDER_NAME = r.PROVIDER_NAME,
+          PROVIDER_DESCRIPTION = r.PROVIDER_DESCRIPTION
+      WHERE PROVIDER_USER_ID = r.PROVIDER_USER_ID;
+      v_count := v_count + SQL%ROWCOUNT;
+    END LOOP;
+    DBMS_OUTPUT.PUT_LINE('Registros actualizados en PROVIDER: ' || v_count);
+  END LOOP;
+END;
+/
